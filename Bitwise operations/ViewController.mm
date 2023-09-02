25776b42100e5e95f1eb29c4fc3a0fd841463242
IRU <ilya.rusalovskiy@mail.ru>
2023-09-03 00:15:03 +0300

    fixes for scheme; check API

--- Bitwise operations/ViewController.mm
+++ Bitwise operations/ViewController.mm
@@ -35,7 +35,7 @@ using namespace std;
 // Plaintext prime modulus
 unsigned long p = 2;
 // Cyclotomic polynomial - defines phi(m)
-unsigned long m = 128; // this will give 32 slots
+unsigned long m = 3; // this will give 1 slot
 // Hensel lifting (default = 1)
 unsigned long r = 1;
 // Number of bits of the modulus chain
@@ -45,26 +45,76 @@ unsigned long c = 2;
 // Size of NTL thread pool (default =1)
 unsigned long nthreads = 12;
 // debug output (default no debug output)
-unsigned long debug = 0;
+unsigned long debug = 1;
 
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
+    dispatch_async(dispatch_get_global_queue( QOS_CLASS_USER_INITIATED, 0), ^(void){
+        [self bitwiseSumm];
+    });
 }
 
 - (void)bitwiseSumm {
     long left = 10;
     long right = 5;
     
-    NSArray *leftBits = @[@0,@0,@0,@0,@0,@0,@0,@0,@0,@0,@0,@0,@1,@0,@1,@0];
-    NSArray *rightBits = @[@0,@0,@0,@0,@0,@0,@0,@0,@0,@0,@0,@0,@0,@1,@0,@1];
+    std::vector<int> leftBits{0,0,0,0,0,0,0,0,0,0,0,0,1,0,1,0};
+    std::vector<int> rightBits{0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,1};
     
     // set NTL Thread pool size
     if (nthreads > 1) {
         NTL::SetNumThreads(nthreads);
     }
     
+    // To setup helib using the hebase layer, let's first
+    // copy all configuration params to an HelibConfig object:
+    HelibConfig conf;
+    conf.p = p;
+    conf.m = m;
+    conf.r = r;
+    conf.L = bits;
+    conf.c = c;
     
+    // Next we'll initialize a BGV scheme in helib.
+    // The following two lines perform full intializiation
+    // Including key generation.
+    HelibBgvContext he;
+    he.init(conf);
+    
+    cout << "\nNumber of slots: " << he.slotCount() << endl;
+    
+    // The encoder class handles both encoding and encrypting.
+    Encoder encoder(he);
+    
+    vector<CTile> encLeftBits;
+    for (const auto& bit : leftBits) {
+        CTile encryptedBit(he);
+        PTile encodedBit(he);
+        encoder.encode(encodedBit, bit);
+        encoder.encrypt(encryptedBit, encodedBit);
+        encLeftBits.push_back(encryptedBit);
+    }
+    
+    vector<CTile> encRightBits;
+    for (const auto& bit : rightBits) {
+        CTile encryptedBit(he);
+        PTile encodedBit(he);
+        encoder.encode(encodedBit, bit);
+        encoder.encrypt(encryptedBit, encodedBit);
+        encRightBits.push_back(encryptedBit);
+    }
+    
+    for(int i = 0; i < encLeftBits.size(); i++) {
+        encLeftBits[i].add(encRightBits[i]);
+    }
+    
+    NSMutableArray *resultArray = [NSMutableArray new];
+    for (const auto& res : encLeftBits) {
+        int resultBit = encoder.decryptDecodeInt(res)[0];
+        [resultArray addObject:@(resultBit)];
+    }
+    NSLog(@"Operation result is: %@", resultArray);
 }
 
 
